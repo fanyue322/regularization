3521a6b4ebd76bb8be14f598a5cbe884eaabf08f
fanyue322 <34554433+fanyue322@users.noreply.github.com>
2021-06-21 20:38:22 +0800

    Update The_bayesian_lasso.m

--- The_bayesian_lasso.m
+++ The_bayesian_lasso.m
@@ -1,86 +1,48 @@
-function beta=The_bayesian_lasso(x,y,n,p)
-
-
-
-
-%betac_draw=zeros(6000,p);
-%m=zeros(p,1);
-%betac=mvnrnd(m,eye(p));
-%betac=betac';
+function z=Bayesian_lasso(x,y)
+burn_in=3000;
+burn_out=3000;
+z=starty(0,1,y);
+[n,p]=size(x);
 tau2=ones(p,1);
-sigma2=0.1;
+sigma2=1;
 
+
+m=zeros(p,1);
+betac=mvnrnd(m,eye(p));
 
 lambda=1;
-%lambda=ones(p,1);
-%lambda(seq)=0.01;
-for iter=1:6000
 
-%     for i=1:p
-%         x_g=x(:,i);
-%         x_dg=x;
-%         x_dg(:,i)=[];
-%         betac_g=betac(i);
-%         betac_dg=betac;
-%         betac_dg(i)=[];
-%         sigmag=inv(x_g'*x_g+1/tau2(i));
-%         miug=sigmag*x_g'*(y-x_dg*betac_dg);
-% 
-% 
-%             betac(i)=normrnd(miug,sigmag*sigma2);
-% 
-%     end
-    D=diag(tau2);
- 
+for iter=1:3000
+
+      D=diag(tau2);
+
    sigmag =inv( x'*x + inv(D));
 
-	betac = mvnrnd(sigmag*x'*y,sigma2*sigmag)';
-   
-  %  for i=1:p
- 
-%             pmu=lambda*sqrt(sigma2)/norm(betac(i));
-%             plambda=lambda^2;
-%             pd = makedist('InverseGaussian','mu',pmu,'lambda',plambda);
-%             temp = random(pd);
-%             if temp==0
-%                 temp=0.01;
-%             end
-%             tau2(i)=1/temp;
-%           end
-        for i=1:p
+	betac = mvnrnd(sigmag*x'*z,sigma2*sigmag)';
+    
+      for i=1:p
 par1=lambda^2;
 par2=betac(i)^2/sigma2;
 tau2(i)=gigrnd(0.5,par1,par2,1);
 
-        end
-            
+      end
+          par1=p+0.01;
+    par2=sum(tau2)/2+0.01;
+    temp=sqrt(gamrnd(par1,1/par2));
+      lambda=temp;
       
 
     
-    par1=p+0.01;
-    par2=sum(tau2)/2+0.01;
-    temp=sqrt(gamrnd(par1,1/par2));
-%    lambda=ones(p,1)*temp;
-  
-  lambda=temp;
+          miu=x*betac;
+    sigmaG=eye(n)*sigma2;
+    z=multruncn(miu,sigmaG,z,y);
     
- %   lambda(seq)=0.01;
     
-    D=diag(tau2);
-%    temp=sum(gamma);
-    p_alpha=n/2+p/2+0.01;
-    p_beta=((y-x*betac)'*(y-x*betac)+betac'*inv(D)*betac)/2+0.01;
-    temp2=gamrnd(p_alpha,1/p_beta);
-    sigma2=1/temp2;
     
+ beta_draw(iter,:)=betac;
 
-
-    beta_draw(iter,:)=betac;
 
 end
+z=mean(beta_draw(burn_in+1:burn_in+burn_out,:));
 
-
-beta=mean(beta_draw(2001:6000,:));
-
-
-
+end
